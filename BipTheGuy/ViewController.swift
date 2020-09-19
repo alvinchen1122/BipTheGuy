4163bd1b78a92314b1de1ca8141190caae98bd63
Alvin Chen <chenbog@bc.edu>
2020-09-19 19:14:51 -0400

    Initial Commit

---
+++ BipTheGuy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BipTheGuy
+//
+//  Created by Alvin Chen on 9/19/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
